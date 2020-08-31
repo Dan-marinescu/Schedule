ffce108941c2009b935c93baa80b0927eb0e4c0f
Dan marinescuעןא <kaskason2@gmail.com>
2020-08-31 12:21:58 +0300

    Few refactors and changes

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
+const days = ['Saturday','Friday','Thursday','Wednesday','Tuesday','Monday','Sunday','']
+const shift = ['Morning','Afternoon','Night']
 
-const generateSchedaule = () => {
+const generateOverallSchedaule = () => {
     const m = []
     const wrapper = document.getElementById('overall')
-    const days = ['Saturday','Friday','Thursday','Wednesday','Tuesday','Monday','Sunday','']
-    const shift = ['Morning','Afternoon','Night']
-    const colors = ['red','green','blue']
-    // var css = 'table td:hover{ background-color: #00ff00 }';
-    var css = 'div :hover{ background-color: #ff0000; background-color: #09ff00;   }';
-
     for(let i = 0 ; i<8 ; i++){
         const div = document.createElement('div');
         div.innerHTML = days[i]
@@ -16,12 +12,8 @@ const generateSchedaule = () => {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 7; j++) {
             const div = document.createElement('div');
-            div.className = 'ite2';
-            // const btn = document.createElement('button')
-            // div.style.backgroundColor =colors[i%3];
-            div.innerHTML = '<button class="btnRemove" value ="'+(i*10+j)+'" onclick="clickRemove('+ 'this'+')" >X</button>'
-            div.innerHTML += '<button class="btnAccept" value ="'+(i*10+j)+'" onclick="clickAccept('+ 'this'+')" >V</button>'
-            // div.appendChild(btn)
+            div.className = 'pers';
+            div.innerHTML = ''
             wrapper.appendChild(div)
             m.push(div)
         }
@@ -31,74 +23,185 @@ const generateSchedaule = () => {
     }
     return m
 }
-console.log(document.getElementById('admin'))
-const typeOfGrids = ['overall','admin','personal']
-const b = generateSchedaule(typeOfGrids[0])
-for(let i = 0 ; i<b.length;i++){
-    console.log(b[i].innerHTML)
-}
 
-// console.log(generateSchedaule(typeOfGrids[1]))
-// console.log(generateSchedaule(typeOfGrids[2]))
-
-
-function shiftsChange(action, res) {
-    var x = parseInt(document.getElementById(res).innerHTML);
-    if (action == "up") {
-        x += 1;
-    } else {
-        if (x > 0) {
-            x -= 1;
-        }
+const generatePersonalSchedaule = () => {
+    const m = []
+    const wrapper = document.getElementById('personal')
+    var counter = 0;
+    for(let i = 0 ; i<8 ; i++){
+        const div = document.createElement('div');
+        div.style.backgroundColor = 'rgb(180,180,180)'
+        div.innerHTML = days[i]
+        wrapper.appendChild(div)
     }
-
-    document.getElementById(res).innerHTML = x;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 7; j++) {
+            const div = createDiv(counter);
+            wrapper.appendChild(div)
+            m.push(div)
+            // removeShiftOption(counter);
+        }
+        const time = document.createElement('div')
+        time.style.backgroundColor = 'rgb(180,180,180)'
+        time.innerHTML = shift[i]
+        wrapper.appendChild(time)
+    }
+    return m
 }
+
+function createDiv(val){
+    const div = document.createElement('div');
+    const btn = document.createElement('button');
+    btn.classList.add("btnRemove","ite1");
+    btn.value = val;
+    btn.addEventListener("click",function (){
+        if(div.className =='accept'){
+            this.classList.remove("btnAccept","accept");
+            this.classList.add("btnRemove","remove");
+            div.className = 'remove';
+
+        }else{
+            this.classList.remove("btnRemove","remove");
+            this.classList.add("btnAccept","accept");
+            div.className = 'accept';
+        }
+    });
+    div.className = 'remove';
+    div.appendChild(btn);
+    return div;
+}
+
+
+
+const generateAdminSchedaule = () => {
+    const m = []
+    const wrapper = document.getElementById('admin')
+    var counter = 0
+    for(let i = 0 ; i<8 ; i++){
+        const div = document.createElement('div');
+        div.style.backgroundColor = 'rgb(180,180,180)';
+        div.innerHTML = days[i];
+        wrapper.appendChild(div);
+    }
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 7; j++) {
+            const div = document.createElement('div');
+            const btnup = document.createElement('button');
+            const btndown = document.createElement('button');
+            btndown.classList.add("adminClass");
+            btnup.classList.add("adminClass");
+            btnup.value = counter;
+            btndown.value = counter;
+            btndown.style.background ="url(images/down.jpg)"; 
+            btnup.style.background ="url(images/up.jpg)"; 
+            div.innerHTML = '<span> <div id="'+counter+'">0</div> </span>' 
+            div.appendChild(btnup)
+            div.appendChild(btndown)
+            removeShiftOption(counter);
+            btnup.addEventListener("click",function(){
+                var x = parseInt(document.getElementById(this.value).innerHTML);
+                x += 1;
+                document.getElementById(this.value).innerHTML = x;
+                overallArray[this.value].style.backgroundColor ="white";
+                addShiftOption(this.value);
+            });
+            btndown.addEventListener("click",function(){
+                var x = parseInt(document.getElementById(this.value).innerHTML);
+                if (x > 0) {
+                    x -= 1;
+                    if(x==0){
+                        removeShiftOption(this.value);
+                        overallArray[this.value].style.backgroundColor ='rgb(180,180,180)';
+                    }
+                }
+                document.getElementById(this.value).innerHTML = x;
+            });
+            wrapper.appendChild(div)
+            m.push(div)
+            counter++
+        }
+        const time = document.createElement('div')
+        time.style.backgroundColor = 'rgb(180,180,180)'
+
+        time.innerHTML = shift[i]
+        wrapper.appendChild(time)
+    }
+    return m
+}
+const personalArray = generatePersonalSchedaule();
+const overallArray = generateOverallSchedaule();
+const adminArray = generateAdminSchedaule();
+
+showArray(personalArray);
+function showArray(x){
+    for(var i=0;i<x.length;i++){
+        console.log(x[i]);
+    }
+}
+
+
+
+function removeShiftOption(num){
+    personalArray[num].className ='';
+    if(typeof personalArray[num].getElementsByClassName('btnAccept')[0]==="undefined")
+        personalArray[num].getElementsByClassName('btnRemove')[0].style.display ='none'; 
+    else{
+        personalArray[num].getElementsByClassName('btnAccept')[0].style.display ='none'; 
+    }
+    personalArray[num].style.backgroundColor ='rgb(180,180,180)';
+}
+function addShiftOption(num){
+    personalArray[num].className ='';
+    personalArray[num].style.backgroundColor ='';
+
+    personalArray[num].classList.add('remove');
+    if(typeof personalArray[num].getElementsByClassName('btnAccept')[0]==="undefined")
+         personalArray[num].getElementsByClassName('btnRemove')[0].style.display ='block'; 
+    else{
+        personalArray[num].getElementsByClassName('btnAccept')[0].style.display ='block'; 
+    }
+    // else
+    
+}
+
+
+
+var fName = document.getElementById("fname");
+fName.addEventListener("change",function() {
+    console.log(this.value);
+});
 
 const modes = ['personal', "overall", "admin"]
 
 function modeType(type) {
     document.getElementById(type).style.display = "grid";
-
     modes.filter(m => m !== type).forEach(m => {
         document.getElementById(m).style.display = "none";
     })
 }
 
-function clickAccept(btnObj) {
-    console.log(btnObj.className)
-    console.log(btnObj.value)
-    console.log(btnObj.parentElement)
+// function clickAccept(btnObj) {
+//     console.log(btnObj.className)
+//     console.log(btnObj.value)
+//     console.log(btnObj.parentElement)
+//     btnObj.style.display = "none";
+//     btnObj.parentElement.querySelectorAll('.btnRemove')[0].style.display = "block";
+//     btnObj.parentElement.className = 'ite2';
 
-    // document.getElementsByClassName(className)[0].style.background = 'red';
-    // btnObj.parentElement.style.background = 'red';
+// }
 
-    btnObj.style.display = "none";
-    // btnObj.style.display = "block";
-    // document.querySelectorAll(`.${btnObj} .btnRemove`)[0].style.display = "none";
-    // document.querySelectorAll(`.${btnObj} .btnAccept`)[0].style.display = "block";
+// function clickRemove(btnObj) {
+//     console.log(btnObj.value)
+//     console.log(btnObj.className)
+//     btnObj.style.display = "none";
+//     btnObj.parentElement.querySelectorAll('.btnAccept')[0].style.display = "block";
+//     btnObj.parentElement.className = 'ite1';
 
-    btnObj.parentElement.querySelectorAll('.btnRemove')[0].style.display = "block";
-    // btnObj.parentElement.style.background = "red";
-    btnObj.parentElement.className = 'ite2';
+//     // document.getElementsByClassName(className)[0].style.background = 'green';
+//     // btnObj.style.background = '#09ff00';
+//     // btnObj.style.display = "block";
+// }
 
-}
-
-function clickRemove(btnObj) {
-    console.log(btnObj.value)
-
-    console.log(btnObj.className)
-    // document.getElementsByClassName(className)[0].style.background = 'green';
-    // btnObj.style.background = '#09ff00';
-
-    btnObj.style.display = "none";
-    btnObj.parentElement.querySelectorAll('.btnAccept')[0].style.display = "block";
-    btnObj.parentElement.className = 'ite1';
-
-
-    // btnObj.style.display = "block";
-
-}
 // function clickAccept(className) {
 //     console.log(className)
 //     var x = document.querySelectorAll(`.${className} .btnAccept`);
